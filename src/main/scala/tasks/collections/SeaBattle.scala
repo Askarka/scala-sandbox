bd57f10e574546142f005c44aeeba8427a610236
askar <askarkaka@icloud.com>
2022-05-23 12:44:47 +0300

    SeaBattle validation completed

--- src/main/scala/tasks/collections/SeaBattle.scala
+++ src/main/scala/tasks/collections/SeaBattle.scala
@@ -1,5 +1,6 @@
 package tasks.collections
 
+import scala.annotation.tailrec
 import scala.io.StdIn
 
 object Naval {
@@ -21,7 +22,7 @@ object SeaBattle {
 
   def validateShip(ship: Ship): Boolean = {
     def matching(acc: Int, x: Int): Int = (acc: Int, x: Int) match {
-      case (_, n) if (n < 0 && n > 9) => println(1)
+      case (_, n) if n < 0 && n > 9 => println(1)
         -2
       case (-2, _) => -2
       case (-1, n) => n
@@ -32,7 +33,7 @@ object SeaBattle {
     ship match {
       case ship if ship.length >= 5 => false
       case ship if ship.length == 1 => true
-      case head :: tail if !(tail.forall(a => a._1 == head._1) ^ tail.forall((a) => a._2 == head._2)) => false
+      case head :: tail if !(tail.forall(a => a._1 == head._1) ^ tail.forall(a => a._2 == head._2)) => false
       case head :: tail if tail.forall(a => a._1 == head._1) => if (
         (head :: tail).flatMap(b => List(b._2)).sorted.foldLeft(-1)(matching) < 0) false else true
       case head :: tail if tail.forall(a => a._2 == head._2) => if (
@@ -42,7 +43,7 @@ object SeaBattle {
   } // определить, подходит ли корабль по своим характеристикам
 
   def validatePosition(ship: Ship, field: Field): Boolean = {
-    ship.forall(a => field(a._1)(a._2))
+    ship.forall(a => !field(a._1)(a._2))
   } // определить, можно ли его поставить
 
   def enrichFleet(fleet: Fleet, name: String, ship: Ship): Fleet = {
@@ -68,7 +69,7 @@ object SeaBattle {
   } // пометить клетки, которые занимает добавляемый корабль
 
   def tryAddShip(game: Game, name: String, ship: Ship): Game = {
-    if (!validateShip(ship) || !validatePosition(ship, game._1))game
+    if (!validateShip(ship) || !validatePosition(ship, game._1)) game
     else {
       new Game(markUsedCells(game._1, ship), enrichFleet(game._2, name, ship))
     }
@@ -77,16 +78,28 @@ object SeaBattle {
   def main(args: Array[String]): Unit = {
     val potentialShipsAmount = StdIn.readLine().toInt
 
+    @tailrec
     def play(game: Game, ships: Int): Game = {
-      ships match {
-        case 0 => game
-        case n: Int => {
-          val name = StdIn.readLine().toInt
-          play(tryAddShip(game, ???, ???), n - 1)
+      @tailrec
+      def readPoints(pointsLeft: Int, ship: Ship): Ship = {
+        pointsLeft match {
+          case 0 => ship
+          case _ =>
+            val points = StdIn.readLine().split(' ')
+            readPoints(pointsLeft - 1, ship :+ new Point(points(0).toInt, points(1).toInt))
         }
       }
+      ships match {
+        case 0 => game
+        case n: Int =>
+          val nameAndPoints = StdIn.readLine().split(' ')
+          val name = nameAndPoints(0)
+          val points = nameAndPoints(1).toInt
+          val ship = readPoints(points, List[Point]())
+          val r = tryAddShip(game, name, ship)
+          play(r, n - 1)
+      }
     }
-
-    play((field, Map[String, Ship]()), potentialShipsAmount)
+    play((field, Map[String, Ship]()), potentialShipsAmount)._2.foreach(n => println(n._1))
   }
 }
