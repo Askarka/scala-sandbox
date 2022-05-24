130adcc0bdefe42fe1a67ee440315fb85dccd1b7
askar <askarkaka@icloud.com>
2022-05-24 13:02:10 +0300

    DiffListImpl added

--- src/main/scala/tasks/collections/DiffListImpl.scala
+++ src/main/scala/tasks/collections/DiffListImpl.scala
@@ -1,9 +1,11 @@
 package tasks.collections
 
+import scala.collection.immutable.Nil
+
 final class DiffListImpl[A](listFunc: List[A] => List[A]) extends DiffList[A](listFunc) {
-  override def prepend(s: List[A]): DiffList[A] = ???
+  override def prepend(s: List[A]): DiffList[A] = new DiffListImpl[A](listFunc.andThen(a => s ++ a))
 
-  override def append(s: List[A]): DiffList[A] = ???
+  override def append(s: List[A]): DiffList[A] = new DiffListImpl[A](listFunc.andThen(a => a ++ s))
 
-  override def result: List[A] = ???
+  override def result: List[A] = listFunc(Nil)
 }
---
+++ src/main/scala/tasks/collections/buffer.sc
@@ -0,0 +1,8 @@
+import tasks.collections.DiffListImpl
+
+val l1 = List(1,2,3)
+val l2 = List(4,5,6)
+//val dl = new DiffListImpl[Int](identity)
+//
+//val result = dl.append(l2).prepend(l1).result // List(1,2,3,4,5,6)
+
